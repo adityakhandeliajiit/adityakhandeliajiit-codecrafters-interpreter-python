a35232dffe4258f79da0dd6abff5f7a811873206
Aditya Khandelia <9923103028@mail.jiit.ac.in>
2025-02-02 15:52:34 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,104 @@
 import sys
 import re
+class Parser:
+    def __init__(self,tokens):
+        self.tokens=tokens
+        self.current=0
+    def parse(self):
+        return self.expression()
+    def expression(self):
+        return self.primary()
+    def primary(self):
+        if self.match("TRUE"):
+            return Literal(True)
+        if self.match("FALSE"):
+            return Literal(False)
+        if self.match("NIL"):
+            return Literal(None) 
+    def match(self,*types):
+        for type in types:
+            if self.check(type):
+                self.advance()
+                return True
+            else:
+                return False
+    def check(self,type):
+        if self.is_at_end():
+            return False
+        return self.peek().type==type
+    def advance(self):
+        if not self.is_at_end():
+            self.current+=1
+        return self.previous()
+    def is_at_end(self):
+        return self.peek().type == "EOF"
+    def peek(self):
+        return self.tokens[self.current]
+    def previous(self):
+        return self.tokens[self.current - 1]  
+class Expr:
+    def accept(self, visitor):
+        pass
 
+class Literal(Expr):
+    def __init__(self, value):
+        self.value = value
+
+    def accept(self, visitor):
+        return visitor.visit_literal_expr(self)
+
+class Binary(Expr):
+    def __init__(self, left, operator, right):
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor):
+        return visitor.visit_binary_expr(self)
+
+class Grouping(Expr):
+    def __init__(self, expression):
+        self.expression = expression
+
+    def accept(self, visitor):
+        return visitor.visit_grouping_expr(self)
+
+class Unary(Expr):
+    def __init__(self, operator, right):
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor):
+        return visitor.visit_unary_expr(self)
+class AstPrinter:
+    def print(self, expr):
+        return expr.accept(self)
+
+    def visit_literal_expr(self, expr):
+        if expr.value is None:
+            return "nil"
+        return str(expr.value)
+
+    def visit_binary_expr(self, expr):
+        return self.parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+    def visit_grouping_expr(self, expr):
+        return self.parenthesize("group", expr.expression)
+
+    def visit_unary_expr(self, expr):
+        return self.parenthesize(expr.operator.lexeme, expr.right)
+
+    def parenthesize(self, name, *exprs):
+        builder = []
+
+        builder.append("(")
+        builder.append(name)
+        for expr in exprs:
+            builder.append(" ")
+            builder.append(expr.accept(self))
+        builder.append(")")
+
+        return "".join(builder)                                                                                
 def extract_word(sentence, start_index):
     end_index = start_index
     while end_index < len(sentence) and (sentence[end_index].isalnum() or sentence[end_index] == "_"):
@@ -15,12 +113,23 @@ def main():
     command = sys.argv[1]
     filename = sys.argv[2]
 
-    if command != "tokenize":
+    if command not in ["tokenize","parse"]:
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
-
+    
     with open(filename) as file:
         file_contents = file.read()
+    if command == "tokenize":
+        pass
+    elif command == "parse":
+        tokens = []
+        
+        parser = Parser(tokens)
+        expression = parser.parse()
+
+        # Print the AST
+        printer = AstPrinter()
+        print(printer.print(expression))    
 
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
