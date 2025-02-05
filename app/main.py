7fcc9bb5f56b9c56b556fd0156c8772fefc332ef
Aditya Khandelia <9923103028@mail.jiit.ac.in>
2025-02-06 00:02:39 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -17,19 +17,19 @@ class Parser:
         return self.primary()
 
     def primary(self):
-     if self.match("TRUE"):
-        return Literal(True)
-     if self.match("FALSE"):
-        return Literal(False)
-     if self.match("NIL"):
-        return Literal(None)
-     if self.match("IDENTIFIER"):  # <-- now check identifiers right after booleans/nil
-        return Literal(self.previous().lexeme)
-     if self.match("NUMBER"):
-        return Literal(self.previous().literal)
-     if self.match("STRING"):
-        return Literal(self.previous().literal)  # Pass the literal value
-    raise Exception("Expected expression")
+        if self.match("TRUE"):
+            return Literal(True)
+        if self.match("FALSE"):
+            return Literal(False)
+        if self.match("NIL"):
+            return Literal(None)
+        if self.match("IDENTIFIER"):
+            return Literal(self.previous().lexeme)        
+        if self.match("NUMBER"):
+            return Literal(self.previous().literal)
+        if self.match("STRING"):
+            return Literal(self.previous().literal)  # Pass the literal value
+        raise Exception("Expected expression")
 
     def match(self, *types):
         for type in types:
