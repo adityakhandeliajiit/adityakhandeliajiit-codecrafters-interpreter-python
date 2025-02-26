ece99a07d0170be58017c5147b3565d21c32b3f9
Aditya Khandelia <9923103028@mail.jiit.ac.in>
2025-02-26 16:58:50 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -196,7 +196,6 @@ class Interpreter:
         elif op == "*":
             if type(right)==bool or type(right)==str or type(left)==bool or type(left)==str: 
                 exit(70)
-            return float(left * right)
         elif op == "/":
             if type(right)==bool or type(right)==str or type(left)==bool or type(left)==str: 
                 exit(70)
